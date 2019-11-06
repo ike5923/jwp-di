26c4f89f71c362075e124b9ded42f95f8aa7f81b
강연욱 <fiasboc23@gmail.com>
2019-11-06 14:25:55 +0900

    feat: @Configuration, @ComponentScan 스캔

--- nextstep-di/src/main/java/nextstep/di/factory/BeanScanner.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanScanner.java
@@ -1,5 +1,7 @@
 package nextstep.di.factory;
 
+import nextstep.annotation.ComponentScan;
+import nextstep.annotation.Configuration;
 import nextstep.stereotype.Controller;
 import nextstep.stereotype.Repository;
 import nextstep.stereotype.Service;
@@ -12,13 +14,26 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BeanScanner {
-    private static final Class[] COMPONENT_ANNOTATIONS = {Controller.class, Repository.class, Service.class};
+    private static final Class[] COMPONENT_ANNOTATIONS = {Controller.class, Repository.class, Service.class, Configuration.class};
     private final Set<Class<?>> clazz;
-    private final Object[] basePackage;
+    private final Object[] basePackages;
 
-    public BeanScanner(Object... basePackage) {
-        this.basePackage = basePackage;
+    public BeanScanner(Object... basePackages) {
+        this.basePackages = getBasePackages(basePackages);
         this.clazz = getTypesAnnotatedWith(COMPONENT_ANNOTATIONS);
+    }
+
+    private Object[] getBasePackages(Object[] basePackage) {
+        if (basePackage == null || basePackage.length == 0) {
+            Reflections reflections = new Reflections("");
+            basePackage = reflections.getTypesAnnotatedWith(Configuration.class)
+                    .stream()
+                    .filter(clazz -> clazz.isAnnotationPresent(ComponentScan.class))
+                    .map(clazz -> clazz.getAnnotation(ComponentScan.class))
+                    .map(ComponentScan::basePackages)
+                    .toArray();
+        }
+        return basePackage;
     }
 
     public Set<Class<?>> getBeans() {
@@ -27,7 +42,7 @@ public class BeanScanner {
 
     @SuppressWarnings("uncheked")
     private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
-        Reflections reflections = new Reflections(basePackage);
+        Reflections reflections = new Reflections(basePackages);
         return Stream.of(annotations)
                 .map(reflections::getTypesAnnotatedWith)
                 .flatMap(Collection::stream)
--- nextstep-di/src/test/java/nextstep/di/factory/BeanScannerTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanScannerTest.java
@@ -1,9 +1,7 @@
 package nextstep.di.factory;
 
-import nextstep.di.factory.example.JdbcQuestionRepository;
-import nextstep.di.factory.example.JdbcUserRepository;
-import nextstep.di.factory.example.MyQnaServiceImpl;
-import nextstep.di.factory.example.QnaController;
+import nextstep.di.factory.example.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
@@ -13,9 +11,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BeanScannerTest {
 
     @Test
-    void scan_Test() {
+    @DisplayName("생성자에 basePackage 입력 해줬을 경우 스캔 확인")
+    void scan_Test1() {
         BeanScanner beanScanner = new BeanScanner("nextstep.di.factory.example");
         Set<Class<?>> actual = Set.of(QnaController.class, JdbcUserRepository.class, JdbcQuestionRepository.class, MyQnaServiceImpl.class);
+        Set<Class<?>> expected = beanScanner.getBeans();
+
+        assertThat(expected).containsAll(actual);
+    }
+
+    @Test
+    @DisplayName("@ComponentScan 해줬을 경우 스캔 확인")
+    void scan_Test2() {
+        BeanScanner beanScanner = new BeanScanner();
+        Set<Class<?>> actual = Set.of(QnaController.class, JdbcUserRepository.class, JdbcQuestionRepository.class, MyQnaServiceImpl.class, IntegrationConfig.class, ExampleConfig.class);
         Set<Class<?>> expected = beanScanner.getBeans();
 
         assertThat(expected).containsAll(actual);
